c66dab8876a21e72915402ff9d24d9a76c59739f
Jinwook Kim <jinwook.kim@riman.com>
2025-03-24 16:36:16 -0400

    add : @EnableJpaAuditing

--- auth-api/auth-api-master/src/main/java/com/auth/api/master/AuthApiMasterApplication.java
+++ auth-api/auth-api-master/src/main/java/com/auth/api/master/AuthApiMasterApplication.java
@@ -2,8 +2,10 @@ package com.auth.api.master;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AuthApiMasterApplication {
 
     public static void main(String[] args) {
